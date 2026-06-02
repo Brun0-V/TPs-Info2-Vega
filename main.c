84c5dfff1cefd83054b0b031b6541bdb8dea9433
Bruno Vega <brunovega006@gmail.com>
2026-06-02 00:47:42 +0000

    Trabajo finalizado

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "funciones.h"
+#include <stdio.h>
 
 int main(void)
 {
     articulos_t articulos[CANT_ARTICULOS] = {0};
-    printf("Bienvendio al Trabajo Practico N°1 de Info 2\n\n");
+    printf("Bienvenido al Trabajo Practico N°1 de Info 2\n\n");
     importar_articulos(articulos);
     imprimir_articulos(articulos);
     printf("\n------------------------ORDENADO------------------------\n");
